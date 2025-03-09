7c6291ed0eea4ccb9987e31962060b94f821a3e1
gowthamck25 <gowthamck03@gmail.com>
2025-03-09 15:38:36 +0530

    add modal to open input field

--- components/Goalitem.js
+++ components/Goalitem.js
@@ -2,11 +2,15 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
-        <Text style={styles.goalText}>{props.text}</Text>
+    <View style={styles.goalItem}>
+        <Pressable 
+          android_ripple={{color: "#210644"}} 
+          onPress={props.onDeleteItem.bind(this, props.id)}
+          style={({pressed}) => pressed && styles.pressedItem}
+        >
+          <Text style={styles.goalText}>{props.text}</Text>
+        </Pressable>
       </View>
-    </Pressable>
   );
 }
 
@@ -17,9 +21,12 @@ const styles = StyleSheet.create({
     margin: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
-    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
   goalText: {
     color: "white",
+    padding: 8,
   }
 })
